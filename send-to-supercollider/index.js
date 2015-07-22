02a4a340daa232c836c32b0ccba215ddc4ba5278
Colin Clark <colinbdclark@gmail.com>
2015-07-22 12:44:11 -0400

    Adds example of sending OSC messages to SuperCollider's sclang.

---
+++ send-to-supercollider/index.js
@@ -0,0 +1,25 @@
+var osc = require("osc");
+
+var udpPort = new osc.UDPPort({
+    // This is the port we're listening on.
+    localAddress: "127.0.0.1",
+    localPort: 57121,
+
+    // This is where sclang is listening for OSC messages.
+    remoteAddress: "127.0.0.1",
+    remotePort: 57120
+});
+
+// Open the socket.
+udpPort.open();
+
+// Every second, send an OSC message to SuperCollider
+setInterval(function() {
+    var msg = {
+        address: "/hello/from/oscjs",
+        args: [Math.random()]
+    };
+
+    console.log("Sending message", msg.address, msg.args, "to", udpPort.options.remoteAddress + ":" + udpPort.options.remotePort);
+    udpPort.send(msg);
+}, 1000);
