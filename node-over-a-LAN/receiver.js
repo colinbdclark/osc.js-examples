182d9b3fc5f12b0e206d7f686e829874951db3e7
Jascha Narveson <jascha@jaschanarveson.com>
2017-03-25 17:37:33 -0400

    Note the option of using 0.0.0.0 as localAddress

--- node-over-a-LAN/receiver.js
+++ node-over-a-LAN/receiver.js
@@ -2,14 +2,14 @@
 This example shows sending UDP over a LAN between two machines with the network names computerA and computerB.
 
 The main difference when sending over a LAN is that you can't use 127.0.0.1 or "localhost" in the "localAddress" property - 
-you need to use the computer's own LAN address.
+you need to use the computer's own network address, or 0.0.0.0
 */
 
 //This is computerB, receiving OSC from computerA...
 
 var osc = require("osc");
 var udp = new osc.UDPPort({
-    localAddress: "computerB.local",
+    localAddress: "computerB.local", // could also use 0.0.0.0
     localPort: 9999
 });
 
